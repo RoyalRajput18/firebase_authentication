21b9edd109e859032fb595389cec3de87a9dfc3d
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 02:37:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_authentication/sign_in.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignIn(),
+    );
   }
 }
