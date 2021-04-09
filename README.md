e0c6e3cef316970eee2605fd70e916edf351b33a
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-09 17:33:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebase_authentication
+ 
---
+++ android/app/src/main/java/com/example/firebase_authentication/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.firebase_authentication;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
