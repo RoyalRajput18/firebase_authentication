66ccafcc78388fef1552e31ca9fe73e9592cfc85
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 09:02:06 +0530

    Update auth.dart

---
+++ lib/services/auth.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //sign in with email & Password
+
+  //register with email & password
+
+}
