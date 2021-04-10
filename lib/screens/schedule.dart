4a4b6019e7573ab9efb77c8b38221577d7d5f2d5
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 17:54:12 +0530

    Update schedule.dart

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_authentication/components/bottomBar.dart';
 import 'package:flutter/material.dart';
 
 class Schedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('This is a schedule page'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Schedule'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('This is schedule page'),
+          ),
+        ),
+        bottomNavigationBar: BottomBar(),
       ),
     );
   }
