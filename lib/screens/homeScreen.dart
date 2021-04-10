77a89120a1561c248acc217f9dc525c934ef1321
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 17:54:01 +0530

    Update homeScreen.dart

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,85 +1,23 @@
-import 'package:firebase_authentication/screens/aboutUs.dart';
-import 'package:firebase_authentication/screens/profile.dart';
-import 'package:firebase_authentication/screens/schedule.dart';
-import 'package:firebase_authentication/screens/setting.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/components/bottomBar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          items: [
-            BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
-                    ),
-                  );
-                },
-                child: Icon(Icons.home),
-              ),
-            ),
-            BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Profile(),
-                    ),
-                  );
-                },
-                child: Icon(Icons.person),
-              ),
-            ),
-            BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Schedule(),
-                    ),
-                  );
-                },
-                child: Icon(Icons.schedule),
-              ),
-            ),
-            BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Setting(),
-                    ),
-                  );
-                },
-                child: Icon(Icons.settings),
-              ),
-            ),
-            BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AboutUs(),
-                    ),
-                  );
-                },
-                child: Icon(Icons.info),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Welcome'),
         ),
+        body: Container(
+          child: Center(
+            child: Text('Welcome'),
+          ),
+        ),
+        bottomNavigationBar: BottomBar(),
       ),
     );
   }
