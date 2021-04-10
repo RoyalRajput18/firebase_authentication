62fd3a6f00b7bcb04c02a4cd63436aaa7b049e14
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 09:01:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_authentication/screens/sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
 
