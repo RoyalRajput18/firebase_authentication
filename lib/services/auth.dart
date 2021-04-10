5bbe1d3d81cc5788cf210a8acd43d74acf49e7ba
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-10 09:37:43 +0530

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_authentication/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //user object based on firebase user
   bool showSpinner = false;
-  String userName;
   String email;
   String password;
-}
-//sign in with email & Password
+  //sign in with email & Password
 
-//register with email & password
+  //register with email & password
+
+}
