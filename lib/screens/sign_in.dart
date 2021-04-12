f030971f3650972cc1b6ec115283ae3b047ec946
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-12 13:18:55 +0530

    Update sign_in.dart

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -3,6 +3,7 @@ import 'package:firebase_authentication/constants/constants.dart';
 import 'package:firebase_authentication/screens/homeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:wc_form_validators/wc_form_validators.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -14,6 +15,9 @@ class _SignInState extends State<SignIn> {
   bool showSpinner = false;
   String email;
   String password;
+  bool wrongPassword;
+  bool wrongEmail;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,87 +26,102 @@ class _SignInState extends State<SignIn> {
         body: ModalProgressHUD(
           inAsyncCall: showSpinner,
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
-                ),
-                BackArrowIcon(),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TitleText(
-                        title: 'Hello Again! \nWelcome \nback',
-                      ),
-                      SizedBox(
-                        height: 35,
-                      ),
-                      InputTextField(
-                        hintText: 'Email Address',
-                        obscure: false,
-                        onChanged: (value) {
-                          email = value;
-                        },
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      InputTextField(
-                        hintText: 'Password',
-                        obscure: true,
-                        onChanged: (value) {
-                          password = value;
-                        },
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Button(
-                        buttonText: 'Sign In',
-                        onPressed: () async {
-                          setState(() {
-                            showSpinner = true;
-                          });
-                          try {
-                            final user = await _auth.signInWithEmailAndPassword(
-                                email: email, password: password);
-                            if (user != null) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ),
-                              );
-                            }
-                            setState(() {
-                              showSpinner = false;
-                            });
-                          } catch (e) {
-                            print(e);
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      SmallTextUnderButton(
-                        leftSideText: 'Forget your password?',
-                        rightSideText: 'Signup',
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      BottomImage(),
-                    ],
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-              ],
+                  BackArrowIcon(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        TitleText(
+                          title: 'Hello Again! \nWelcome \nback',
+                        ),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        InputTextField(
+                          hintText: 'Email Address',
+                          obscure: false,
+                          validator: Validators.compose([
+                            Validators.required('Email is required'),
+                            Validators.email('Invalid email address'),
+                          ]),
+                          onChanged: (value) {
+                            email = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        InputTextField(
+                          hintText: 'Password',
+                          obscure: true,
+                          validator: Validators.compose([
+                            Validators.required('Password is required'),
+                            Validators.patternString(
+                                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$',
+                                'Invalid Password')
+                          ]),
+                          onChanged: (value) {
+                            password = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Button(
+                          buttonText: 'Sign In',
+                          onPressed: () async {
+                            setState(() {
+                              showSpinner = true;
+                              wrongEmail = false;
+                              wrongPassword = false;
+                            });
+                            try {
+                              final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                      email: email, password: password);
+                              if (user != null) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomeScreen(),
+                                  ),
+                                );
+                              }
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        SmallTextUnderButton(
+                          leftSideText: 'Forget your password?',
+                          rightSideText: 'Signup',
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        BottomImage(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
