29f6b36211eef8eba120bb18a7928a330ba44921
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-13 11:20:10 +0530

    Create setting.dart

---
+++ lib/pages/setup/screens/setting.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_authentication/pages/setup/components/bottomBar.dart';
+import 'package:flutter/material.dart';
+
+class Setting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Setting'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('This is setting page'),
+          ),
+        ),
+        bottomNavigationBar: BottomBar(),
+      ),
+    );
+  }
+}
