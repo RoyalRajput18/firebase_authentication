7d3ddfa0ad3e6faf152718046b836a5be7c28c27
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-15 10:53:42 +0530

    Update register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_authentication/main.dart';
 import 'package:firebase_authentication/constants/constants.dart';
-import 'package:firebase_authentication/screens/sign_in.dart';
+import 'package:firebase_authentication/screens/sign_in_page.dart';
+import 'package:firebase_authentication/services/authentication_services.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:provider/provider.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -23,105 +24,106 @@ class _Register extends State<Register> {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-          child: ModalProgressHUD(
-            inAsyncCall: showSpinner,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              BackArrowIcon(),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TitleText(
+                      title: 'Hello! \nSignup to \nget started',
+                    ),
+                    // SizedBox(
+                    //   height: 15,
+                    // ),
+                    // ClipRRect(
+                    //   borderRadius: BorderRadius.circular(8.0),
+                    //   child: Image(
+                    //     image: AssetImage('images/profile.png'),
+                    //     height: 150.0,
+                    //     width: 100.0,
+                    //   ),
+                    // ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    InputTextField(
+                      controller: nameTextEditingController,
+                      hintText: 'Name',
+                      obscure: false,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputTextField(
+                      controller: emailTextEditingController,
+                      hintText: 'Email Address',
+                      obscure: false,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputTextField(
+                      controller: passwordTextEditingController,
+                      hintText: 'Password',
+                      obscure: true,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Button(
+                      buttonText: 'Sign Up',
+                      onPressed: () {
+                        setState(() {
+                          return LinearProgressIndicator();
+                        });
+                        if (nameTextEditingController.text.length < 3) {
+                          displayToastMessage(
+                              'Name must be at least 3 characters', context);
+                        } else if (!emailTextEditingController.text
+                            .contains('@')) {
+                          displayToastMessage(
+                              'Email address is invalid', context);
+                        } else if (passwordTextEditingController.text.length <
+                            6) {
+                          displayToastMessage(
+                              'Name must be at least 6 characters', context);
+                        } else {
+                          context.read<AuthenticationService>().signUp(
+                                email: emailTextEditingController.text.trim(),
+                                password:
+                                    passwordTextEditingController.text.trim(),
+                              );
+                        }
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SmallTextUnderButton(
+                      leftSideText: 'Already a user ?',
+                      rightSideText: 'sign in',
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    BottomImage(),
+                  ],
                 ),
-                BackArrowIcon(),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TitleText(
-                        title: 'Hello! \nSignup to \nget started',
-                      ),
-                      // SizedBox(
-                      //   height: 15,
-                      // ),
-                      // ClipRRect(
-                      //   borderRadius: BorderRadius.circular(8.0),
-                      //   child: Image(
-                      //     image: AssetImage('images/profile.png'),
-                      //     height: 150.0,
-                      //     width: 100.0,
-                      //   ),
-                      // ),
-                      SizedBox(
-                        height: 35,
-                      ),
-                      InputTextField(
-                        controller: nameTextEditingController,
-                        hintText: 'Name',
-                        obscure: false,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      InputTextField(
-                        controller: emailTextEditingController,
-                        hintText: 'Email Address',
-                        obscure: false,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      InputTextField(
-                        controller: passwordTextEditingController,
-                        hintText: 'Password',
-                        obscure: true,
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Button(
-                        buttonText: 'Sign Up',
-                        onPressed: () {
-                          setState(() {
-                            return LinearProgressIndicator();
-                          });
-                          if (nameTextEditingController.text.length < 3) {
-                            displayToastMessage(
-                                'Name must be at least 3 characters', context);
-                          } else if (!emailTextEditingController.text
-                              .contains('@')) {
-                            displayToastMessage(
-                                'Email address is invalid', context);
-                          } else if (passwordTextEditingController.text.length <
-                              6) {
-                            displayToastMessage(
-                                'Name must be at least 6 characters', context);
-                          } else {
-                            registerNewUser(context);
-                          }
-                          setState(() {
-                            showSpinner = false;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      SmallTextUnderButton(
-                        leftSideText: 'Already a user ?',
-                        rightSideText: 'sign in',
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      BottomImage(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
@@ -154,7 +156,7 @@ class _Register extends State<Register> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => SignIn(),
+          builder: (context) => SignInPage(),
         ),
       );
     } else {
