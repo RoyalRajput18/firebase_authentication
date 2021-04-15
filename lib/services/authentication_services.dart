314b1a4cbd843de6eb7ec3fb32256acb792c4345
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-15 12:15:48 +0530

    Update authentication_services.dart

--- lib/services/authentication_services.dart
+++ lib/services/authentication_services.dart
@@ -10,17 +10,10 @@ class AuthenticationService {
   /// Changed to idTokenChanges as it updates depending on more cases.
   Stream<User> get authStateChanges => _firebaseAuth.idTokenChanges();
 
-  /// This won't pop routes so you could do something like
-  /// Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
-  /// after you called this method if you want to pop all routes.
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
 
-  /// There are a lot of different ways on how you can do exception handling.
-  /// This is to make it as easy as possible but a better way would be to
-  /// use your own custom class that would take the exception and return better
-  /// error messages. That way you can throw, return or whatever you prefer with that instead.
   Future<String> signIn({String email, String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
@@ -30,7 +23,7 @@ class AuthenticationService {
       return e.message;
     }
   }
-  // signInUser(BuildContext context) async {
+  // signInUser({String email, String password}) async {
   //   final User firebaseUser = (await _firebaseAuth
   //           .signInWithEmailAndPassword(email: email, password: password)
   //           .catchError((errMsg) {
@@ -57,10 +50,6 @@ class AuthenticationService {
   //   }
   // }
 
-  /// There are a lot of different ways on how you can do exception handling.
-  /// This is to make it as easy as possible but a better way would be to
-  /// use your own custom class that would take the exception and return better
-  /// error messages. That way you can throw, return or whatever you prefer with that instead.
   Future<String> signUp({String email, String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
