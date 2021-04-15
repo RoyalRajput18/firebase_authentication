0a9654544007a17bbff9532c1fe830e5709cf7c5
Divyarajsinh18 <80568688+Divyarajsinh18@users.noreply.github.com>
2021-04-15 10:53:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:firebase_authentication/screens/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/screens/homeScreen.dart';
+import 'package:firebase_authentication/screens/sign_in_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'services/authentication_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +19,32 @@ DatabaseReference userRef = FirebaseDatabase.instance.reference().child('user');
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SignIn(),
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthenticationWrapper(),
+      ),
     );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomeScreen();
+    }
+    return SignInPage();
   }
 }
